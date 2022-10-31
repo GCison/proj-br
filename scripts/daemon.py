24f3b3cf654df3e829fbbcd1be35dea23c563ceb
GCIson <cison@email.com>
2022-10-31 05:39:30 -0300

    atualiza projecao

--- scripts/daemon.py
+++ scripts/daemon.py
@@ -1,8 +1,10 @@
 import time
 import os
-
+import sys
 
 while True:
+    time_update:int = sys.argv[1:]
+
     os.system("~/cison/html/proj-br/scripts/worker_update.sh")
-    time.sleep(60)
+    time.sleep(time_update)
 
